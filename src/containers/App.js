d2d7e2181a7b28e42209222e9aa88084d90a2f5a
eyilmaz <eyilmaz@ziraatteknoloji.com>
2018-06-28 10:49:17 +0300

    efdal reads questions from json file

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,39 +3,24 @@ import './App.css';
 import AnswerButton from '../components/AnswerButton';
 import {Row, Col} from 'antd';
 import CodeMirror from "react-codemirror";
+const jsonData = require('../data/Questions.json');
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            Questions : jsonData.Questions
+        };
+    }
 
 
     render() {
-        let question = `class ParentClass {
-                            getCount() {
-                                return 20;
-                            }
-                        }
-                        
-                        class ChildClass extends ParentClass {
-                            getCount() {
-                                return 30;
-                            }
-                        }
-                        
-                        let c = new ChildClass();
-                        console.log(c.getCount());
-                        
-                        let c = new ChildClass();
-                        console.log(c.getCount());
-                        
-                        let c = new ChildClass();
-                        console.log(c.getCount());
-                        
-                        let c = new ChildClass();
-                        console.log(c.getCount());
-                        
-                        
-                        let c = new ChildClass();
-                        console.log(c.getCount());
-                        `;
+        let questionText =  this.state.Questions[0] ? this.state.Questions[0].text: "";
+        let optionsA =  this.state.Questions[0] ? this.state.Questions[0].optionsA: "";
+        let optionsB =  this.state.Questions[0] ? this.state.Questions[0].optionsB: "";
+
+        console.log(questionText.toString());
+
         var options = {
             lineNumbers: true,
             readOnly: true,
@@ -46,7 +31,7 @@ class App extends Component {
         return (
             <div className="questionCard">
                 <Row className="question">
-                    <CodeMirror value={question}
+                    <CodeMirror value={questionText}
                                 options={options}/>
                 </Row>
                 <Row>
@@ -54,10 +39,10 @@ class App extends Component {
                 </Row>
                 <Row>
                     <Col span={12}>
-                        <AnswerButton text="Answer A"/>
+                        <AnswerButton text={optionsA}/>
                     </Col>
                     <Col span={12}>
-                        <AnswerButton text="Answer B"/>
+                        <AnswerButton text={optionsB}/>
                     </Col>
                 </Row>
             </div>
